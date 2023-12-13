64f146ad8460ba1642203cb47a25d0bfa12afa4b
leiting <leiting@hualala.com>
2023-12-13 10:38:16 +0800

    ts

--- axios/index.tsx
+++ axios/index.tsx
@@ -1,5 +1,5 @@
-import Axios from './Axios.tsx';
-import { AxiosInstance } from './types.tsx'
+import Axios from './Axios';
+import { AxiosInstance } from './types'
 function createInstance() {
     let context: Axios<any> = new Axios();
     let instance: AxiosInstance = Axios.prototype.request.bind(context);
